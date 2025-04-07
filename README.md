50211c3ab3a388a067f79659aad99c1b998bdbe1
lite wong <lite@rootglobal.net>
2025-04-07 16:25:46 +0800

    support external config file and specify mqtt topic to publish message to kafka

--- README.md
+++ README.md
@@ -13,7 +13,13 @@ A EMQX plugin release is a zip package including
 1. A JSON format metadata file
 2. A tar file with plugin's apps packed
 
-Execute `make rel` to have the package created like:
+Ensure you have installed OTP 24+ ,git 2.x ,cmake 3.4+ in your environment.
+
+```bash
+make rel
+```
+
+Execute `make rel` in the project folder , if succeeded ,you will have the package created like:
 
 ```
 _build/default/emqx_plugrel/emqx_plugin_kafka-5.0.0.tar.gz
@@ -21,20 +27,29 @@ _build/default/emqx_plugrel/emqx_plugin_kafka-5.0.0.tar.gz
 
 You can directly install above package to emqx5+ plugins dashboard, and following below guides to setup the env variables.
 
-######kafka configure in /etc/profile
-set up kafka configuration in env file
+Set up the the extenal config file , it is from a fixed path: /etc/emqx_kafka.conf. The following is an example, mqtt_topics defines the mqtt topics to publish to kafka.
+
 ```
-export KAFKA_ADDRESS_LIST=192.168.1.1:9092,192.168.1.2:9092,192.168.1.3:9092
-export KAFKA_TOPIC=mqtt-topic
+kafka {
+  address_list = "127.0.0.1:9092" # kafka address list, multiple addresses are separated by commas
+  reconnect_cool_down_seconds = 15
+  query_api_versions = true
+  topic = "mqtt-events" # kafka topic to publish
+  mqtt_topics  = [ "/data/#" ]
+}
+```
+
+Alternatively, you can set up kafka configuration in /etc/profile, if there is not extenal config, the plugin will read the configuration from /etc/profile.
+
+set up kafka configuration in env file, 
+```
+export KAFKA_ADDRESS_LIST=192.168.1.1:9092 # kafka address list, multiple addresses are separated by commas
+export KAFKA_TOPIC=mqtt-topic # kafka topic to publish
 export KAFKA_QUERY_API_VERSIONS=true
 export KAFKA_RECONNECT_COOL_DOWN_SECONDS=10
+export KAFKA_MQTT_TOPICS=/data/#,/data2/#
 ```
-
-If deploying emqx with build from source.
-
-1. need to set NIF_BIN_DIR to /usr/lib64  
-   text "export NIF_BIN_DIR = /ust/lib64" to  /etc/profile 
-2. put all plugin's  *.so file to /usr/lib64
+Note: If you are using docker, you need to use -e to specify the env variables or  mount the extenal config file to the container.
 
 
 
--- src/emqx_plugin_kafka_app.erl
+++ src/emqx_plugin_kafka_app.erl
@@ -29,19 +29,79 @@ start(_StartType, _StartArgs) ->
     Cnf = get_kafka_config(),
     emqx_plugin_kafka:load(Cnf),
     {ok, Sup}.
-
 stop(_State) ->
     emqx_plugin_kafka:unload().
 
 get_kafka_config() ->
-    %case emqx_conf:get_raw([kafka]) of
-    %     {config_not_found,[kafka]} ->
-            #{
-               address_list => os:getenv("KAFKA_ADDRESS_LIST") ,
-               reconnect_cool_down_seconds => os:getenv("KAFKA_RECONNECT_COOL_DOWN_SECONDS") ,
-               query_api_versions => os:getenv("KAFKA_QUERY_API_VERSIONS") ,
-               topic => os:getenv("KAFKA_TOPIC") 
-            }.
-    %    _ ->
-    %        emqx_conf:get_raw(kafka)
-    %end.
+    FixedPath = "/etc/emqx_kafka.conf",
+    CNF = 
+        case filelib:is_file(FixedPath) of
+            true ->
+               %% {ok, Content} = file:read_file(FixedPath),
+                {ok, Parsed} = hocon:load(FixedPath),
+                logger:info("Config content ~p found, using it", [Parsed]),
+                %% 现在可以使用原子键 kafka
+                case maps:get(<<"kafka">>, Parsed, undefined) of
+                    undefined ->
+                        logger:error("Missing kafka section in config file ~s", [FixedPath]),
+                        fallback_config();
+                    KafkaConfig ->
+                        case maps:get(<<"address_list">>, KafkaConfig, undefined) of
+                            undefined ->
+                                logger:error("Missing address_list in kafka config"),
+                                fallback_config();
+                            _ ->
+                                Config = #{
+                                    address_list =>  maps:get(<<"address_list">>, KafkaConfig, undefined),
+                                    reconnect_cool_down_seconds => maps:get(<<"reconnect_cool_down_seconds">>, KafkaConfig, undefined),
+                                    query_api_versions => maps:get(<<"query_api_versions">>, KafkaConfig, undefined),
+                                    topic => maps:get(<<"topic">>, KafkaConfig, undefined),
+                                    mqtt_topics => maps:get(<<"mqtt_topics">>, KafkaConfig, undefined)
+                                },
+                                application:set_env(emqx_plugin_kafka, kafka, Config),
+                                Config
+                        end    
+                end;
+            false ->
+                logger:warning("Config file ~s not found, using default", [FixedPath]),
+                fallback_config()
+        end,
+    CNF.
+
+fallback_config() ->
+    %% 修改后的环境变量获取方式
+    AddressList = case os:getenv("KAFKA_ADDRESS_LIST") of
+        false -> 
+            %% 添加调试日志
+            logger:debug("KAFKA_ADDRESS_LIST not found in env: ~p", [os:getenv()]),
+            [];
+        "" ->   %% 处理空字符串情况
+            logger:warning("KAFKA_ADDRESS_LIST is empty"),
+            [];
+        Value -> Value
+    end,
+    logger:info("env address list : ~p", [AddressList]),
+    
+    Config = #{address_list => list_to_binary(AddressList),
+      reconnect_cool_down_seconds => list_to_integer(
+          case os:getenv("KAFKA_RECONNECT_COOL_DOWN_SECONDS") of
+              false -> "10";
+              Val -> Val
+          end),
+      query_api_versions => list_to_atom(
+          case os:getenv("KAFKA_QUERY_API_VERSIONS") of
+              false -> "true";
+              Val -> Val
+          end),
+      topic => list_to_binary(case os:getenv("KAFKA_TOPIC") of
+                false -> "mqtt-publish";
+                T -> T
+               end),
+      mqtt_topics => 
+          case os:getenv("KAFKA_MQTT_TOPICS") of
+              false -> undefined;
+              Topics -> list_to_binary(Topics)
+          end
+    },
+    application:set_env(emqx_plugin_kafka, kafka, Config),
+    Config.
